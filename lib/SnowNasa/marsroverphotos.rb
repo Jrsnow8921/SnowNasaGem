d4de1566c96e5c344b21c52960e765acfa8e7021
Justin <jrsnow8921@gmail.com>
2016-06-01 11:35:48 -0400

    spelling -

--- lib/SnowNasa/marsroverphotos.rb
+++ lib/SnowNasa/marsroverphotos.rb
@@ -3,13 +3,13 @@ module SnowNasa
     include SnowNasa
 
     def self.cameras
-      fhaz = "(FHAZ)= Front Hazard Avoidance Camera- This is onboard Curiousity, Opportunity, and Spirit\n".red
-      rhaz = "(RHAZ)= Rear Hazard Avoidance Camera- This is onboard Curiousity, Opportunity, and Spirit\n".green
-      mast = "(MAST)= Mast Camera- This is onboard Curiousity only\n".yellow
-      chemcam = "(CHEMCAM)= Chemistry and Camera Complex- This is onboard Curiousity only\n".red
-      mahli = "(MAHLI)= Mars Hand Lens Imager- This is onboard Curiousity only\n".green
-      mardi = "(MARDI)= Mars Descent Imager- This is onboard Curiousity only\n".yellow
-      navcam = "(NAVCAM)= Navigation Camera- This is onboard Curiousity, Opportunity, and Spirit\n".red
+      fhaz = "(FHAZ)= Front Hazard Avoidance Camera- This is onboard Curiosity, Opportunity, and Spirit\n".red
+      rhaz = "(RHAZ)= Rear Hazard Avoidance Camera- This is onboard Curiosity, Opportunity, and Spirit\n".green
+      mast = "(MAST)= Mast Camera- This is onboard Curiosity only\n".yellow
+      chemcam = "(CHEMCAM)= Chemistry and Camera Complex- This is onboard Curiosity only\n".red
+      mahli = "(MAHLI)= Mars Hand Lens Imager- This is onboard Curiosity only\n".green
+      mardi = "(MARDI)= Mars Descent Imager- This is onboard Curiosity only\n".yellow
+      navcam = "(NAVCAM)= Navigation Camera- This is onboard Curiosity, Opportunity, and Spirit\n".red
       pancam = "(PANCAM)= Panoramic Camera- This is onboard Opportunity, and Spirit\n".green
       minites = "(MINITES)= Minature Thermal Emission Spectrometer(Mini-TES)- This in onbard Opportunity, and Spirit\n".yellow
       cams =(
